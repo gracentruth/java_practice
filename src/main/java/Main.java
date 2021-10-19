e660dc495d988bbc1fefa57db93af1e094480220
gracentruth <63465350+gracentruth@users.noreply.github.com>
2021-10-19 17:12:12 +0900

    ArrayList practice1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class Main {
-    int a;
+    public static void main(String[] args){
+
+        //어레이리스트 생성
+        ArrayList<String> nameList=new ArrayList<String>();
+
+        //엘레멘트 추가
+        nameList.add("dog"); //인덱스 0부터 시작
+        nameList.add("cat");
+        nameList.add("bear");
+        nameList.add("rabit");
+        //크기
+        System.out.println("크기: "+nameList.size());
+
+        //특정 인덱스 요소 가져오기
+        System.out.println("2번째 요소"+nameList.get(1));
+        //특정 인덱스 요소 삭제하기
+        nameList.remove(0);
+
+        for(int i=0;i<nameList.size();i++){
+           System.out.println((i+1)+". "+nameList.get(i));
+        }
+
+        for(String n : nameList){
+            System.out.println(n);
+        }
+
+
+  }
+
 }
